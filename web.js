4e6ac3edfc5f2eb874f6a8fe29f0c31972da5ff8
milan katwal <milankatwal2056@gmail.com>
2024-02-01 10:47:16 +0545

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(request, response) {
-  response.send(takeover by miky!");
+  response.send("Hello World!");
 });
 
 app.get("/test", function(request, response) {
