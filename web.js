6d935ac5505b945840bf45f6529b5be7ec22eb90
milan katwal <milankatwal2056@gmail.com>
2024-02-01 10:38:15 +0545

    Create web.js

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(request, response) {
+  response.send(takeover by miky!");
+});
+
+app.get("/test", function(request, response) {
+  response.send("Hello TEST!");
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
